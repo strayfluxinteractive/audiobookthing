aec03228e11836a2bf5e576517a813afc16974a8
zephyrz4 <zephyrz2712@gmail.com>
2016-04-02 15:26:58 -0400

    add env config

---
+++ config.go
@@ -0,0 +1,8 @@
+package main
+
+// Spec represents the configuration.
+type Spec struct {
+	Debug bool
+}
+
+var spec Spec
--- server.go
+++ server.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
 func main() {
+	//Load configuration variables
+	err := envconfig.Process("audiobookthing", &spec)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Start HTTP server
 	router := NewRouter()
-
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
